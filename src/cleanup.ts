cf38f96dac0112101ca80903575cc32df76f90fe
trev <me@trev.xyz>
2025-08-10 02:48:53 -0400

    feat: use nix copy

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -3,18 +3,22 @@ import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 import { getKey } from "./key.js";
 
-try {
+async function main() {
+	// Optimise the nix store
+	await exec.exec("nix", ["store", "optimise"]);
+
 	// Export nix store
-	await exec.exec("bash", [
-		"-c",
-		"nix-store --export $(find /nix/store -maxdepth 1 -name '*-*') > /tmp/nixcache",
-	]);
+	await exec.exec("nix", ["copy", "--all", "--to", "file:///tmp/nix-cache"]);
 
 	// Get cache key
 	const key = await getKey();
 
 	// Save nix store to cache
-	await cache.saveCache(["/tmp/nixcache"], key);
+	await cache.saveCache(["/tmp/nix-cache"], key);
+}
+
+try {
+	await main();
 } catch (error) {
 	if (error instanceof Error) core.setFailed(error.message);
 }
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,24 @@ import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 import { getKey } from "./key.js";
 
-try {
+async function main() {
 	// Get cache key
 	const key = await getKey();
 
 	// Restore cache to tmp
-	const restore = await cache.restoreCache(["/tmp/nixcache"], key);
-
-	// If cache was restored, import it
-	if (restore) {
-		await exec.exec("bash", ["-c", "nix-store --import < /tmp/nixcache"]);
+	const restore = await cache.restoreCache(["/tmp/nix-cache"], key);
+	core.setOutput("cache-hit", restore ? "true" : "false");
+	if (!restore) {
+		core.info("Cache not found, proceeding without restoring.");
+		return;
 	}
 
-	core.setOutput("cache-hit", restore ? "true" : "false");
+	// If cache was restored, import it
+	await exec.exec("nix", ["copy", "--all", "--from", "file:///tmp/nix-cache"]);
+}
+
+try {
+	await main();
 } catch (error) {
 	if (error instanceof Error) core.setFailed(error.message);
 }
