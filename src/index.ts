7a91ad016deb23f86cf706efc0c381dc76289446
trev <me@trev.xyz>
2025-08-11 07:28:38 -0400

    test: individually copy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as cache from "@actions/cache";
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
+import * as glob from "@actions/glob";
 import { getKey } from "./key.js";
 
 async function main() {
@@ -15,15 +16,31 @@ async function main() {
 		return;
 	}
 
-	// If cache was restored, import it
-	await exec.exec("nix", [
-		"copy",
-		"--all",
-		"--from",
-		"file:///tmp/nix-cache",
-		"--no-check-sigs",
-		"--repair",
-	]);
+	// Get all nar files in the cache directory
+	const globber = await glob.create("/tmp/nix-cache/*.narinfo");
+	const narFiles = await globber.glob();
+
+	// If no nar files found, exit early
+	if (narFiles.length === 0) {
+		core.info("No nar files found in the cache, exiting.");
+		return;
+	}
+
+	// Log the number of nar files found
+	core.info(`Found ${narFiles.length} nar files in the cache.`);
+
+	// Import all nar files from the cache
+	// Necessary until https://github.com/NixOS/nix/issues/9052 is resolved
+	for (const narFile of narFiles) {
+		await exec.exec("nix", [
+			"copy",
+			narFile,
+			"--from",
+			"file:///tmp/nix-cache",
+			"--no-check-sigs",
+			"--offline",
+		]);
+	}
 }
 
 try {
