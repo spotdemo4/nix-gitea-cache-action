947d8a43313eb1681c38f04b31031bda827dda52
trev <me@trev.xyz>
2025-08-11 08:20:49 -0400

    fix: try to import all first, and only retry if failed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import * as fs from "node:fs";
 import * as cache from "@actions/cache";
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
-import * as glob from "@actions/glob";
 import { getKey } from "./key.js";
 
 async function main() {
@@ -17,44 +15,42 @@ async function main() {
 		return;
 	}
 
-	// Get all nar files in the cache directory
-	const globber = await glob.create("/tmp/nix-cache/*.narinfo");
-	const narFiles = await globber.glob();
+	const badPaths: string[] = [];
+	while (true) {
+		// If cache was restored, import it
+		const out = await exec.getExecOutput("nix", [
+			"copy",
+			"--all",
+			"--from",
+			"file:///tmp/nix-cache",
+			"--no-check-sigs",
+		]);
 
-	// If no nar files found, exit early
-	if (narFiles.length === 0) {
-		core.info("No nar files found in the cache, exiting.");
-		return;
-	}
+		if (out.exitCode === 0) break;
 
-	// Log the number of nar files found
-	core.info(`Found ${narFiles.length} nar files in the cache.`);
-
-	// Import all nar files from the cache
-	// Necessary until https://github.com/NixOS/nix/issues/9052 is resolved
-	for (const narFile of narFiles) {
-		const path = await getStorePath(narFile);
-		if (!path) {
-			core.warning(`Could not extract store path from ${narFile}, skipping.`);
-			continue;
+		// If the path is not valid, retry
+		// Necessary until https://github.com/NixOS/nix/issues/9052 is resolved
+		const badPath = out.stderr.match(/path '(.+)' is not valid/)?.at(1);
+		if (!badPath) {
+			core.warning("Failed to import cache, but no bad path found. Exiting.");
+			return;
 		}
 
+		if (badPaths.includes(badPath)) {
+			core.warning(`'${badPath}' is not valid, but already retried. Exiting.`);
+			return;
+		}
+
+		badPaths.push(badPath);
+		core.warning(`'${badPath}' is not valid, retrying...`);
 		await exec.exec("nix", [
 			"copy",
-			path,
+			badPath,
 			"--from",
 			"file:///tmp/nix-cache",
 			"--no-check-sigs",
 			"--offline",
 		]);
-	}
-}
-
-async function getStorePath(pathToFile: string) {
-	const fileContent = fs.readFileSync(pathToFile, "utf-8");
-	const storePath = fileContent.match(/^StorePath: (.+)$/m)?.at(1);
-	if (storePath) {
-		return storePath;
 	}
 }
 
